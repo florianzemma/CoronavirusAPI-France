33976570ff1106a970a42dc5f74372b14634d35f
Florian Zemma--gailleton <florianzemma@gmail.com>
2021-11-12 15:17:16 +0100

    change host

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
+  const host = '0.0.0.0';
+  await app.listen(parseInt(process.env.PORT, 10) || 3000, host);
 }
 bootstrap();
