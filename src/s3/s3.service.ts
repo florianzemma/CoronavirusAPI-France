698ed4011ab546f772b0113743746780667d59f7
Florian Zemma--gailleton <florianzemma@gmail.com>
2021-11-22 12:35:15 +0100

    change aws package to v3

--- src/s3/s3.service.ts
+++ src/s3/s3.service.ts
@@ -1,22 +1,38 @@
 import { ConfigService } from '@nestjs/config';
 import { Injectable } from '@nestjs/common';
-import { S3 } from 'aws-sdk';
 import { CovidData } from 'src/data/interface';
+import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
 
 @Injectable()
 export class S3Service {
   constructor(private configService: ConfigService) {}
 
   getFileS3 = async (key: string): Promise<CovidData[]> => {
-    const s3 = new S3();
-    const data = await s3
-      .getObject({
-        Key: key,
-        Bucket: this.configService.get('AWS_BUCKET_NAME'),
-        ResponseContentType: 'application/json',
-      })
-      .promise();
+    const streamToJson = (stream): Promise<unknown> =>
+      new Promise((resolve, reject): void => {
+        const chunks = [];
+        stream.on('data', (chunk): number => chunks.push(chunk));
+        stream.on('error', reject);
+        stream.on('end', (): void =>
+          resolve(JSON.parse(Buffer.concat(chunks).toString('utf8'))),
+        );
+      });
 
-    return JSON.parse(data.Body.toString('utf-8')) as CovidData[];
+    const s3client = new S3Client({
+      credentials: {
+        accessKeyId: this.configService.get('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: this.configService.get('AWS_SECRET_ACCESS_KEY'),
+      },
+      region: this.configService.get('AWS_REGION'),
+    });
+
+    const bucketParams = {
+      Bucket: this.configService.get('AWS_BUCKET_NAME'),
+      Key: key,
+      ResponseContentType: 'application/json',
+    };
+
+    const data = await s3client.send(new GetObjectCommand(bucketParams));
+    return (await streamToJson(data.Body)) as CovidData[];
   };
 }
