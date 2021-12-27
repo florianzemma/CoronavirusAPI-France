e57e4b5724b9847e4bf9d9c883964a95b1480ad1
Florian Zemma--gailleton <florianzemma@gmail.com>
2021-12-27 10:16:16 +0100

    add redis to remove AWS costs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpModule } from '@nestjs/axios';
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import * as redisStore from 'cache-manager-redis-store';
+import { CacheModule, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,15 +9,31 @@ import { DataModule } from './data/data.module';
 import { TaskModule } from './task/task.module';
 import { TaskService } from './task/task.service';
 import { S3Module } from './s3/s3.module';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
     DataModule,
     ScheduleModule.forRoot(),
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        REDIS_URL: Joi.string().required(),
+      }),
+      isGlobal: true,
+    }),
     TaskModule,
     HttpModule,
     S3Module,
+    CacheModule.registerAsync({
+      isGlobal: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        store: redisStore,
+        url: configService.get('REDIS_URL'),
+        ttl: 18000,
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService, TaskService],
