46ca6a1e7e8522af0496fc2e1e199fc7bd73b1ec
Florian Zemma--gailleton <florianzemma@gmail.com>
2021-12-29 09:37:53 +0100

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const host = '0.0.0.0';
   await app.listen(parseInt(process.env.PORT, 10) || 3000, host);
 }
